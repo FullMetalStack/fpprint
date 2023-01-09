d37e7b5c77c0eb50d17dedbf6655d1d716d201ce
John Dodds <john.dodds@populi.uk>
2023-01-09 13:15:40 +0100

    express

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var cors = require("cors");
+
+app.use(cors());
 
 var printJobs = [];
 
-app.get('/api/printJobsAfter/:lastJobId', function (req, res) {
-   var newPrintJobs = printJobs.filter(job => job.id > req.params.lastJobId);
-   res.send(newPrintJobs);
-})
+app.get("/api/printJobsAfter/:lastJobId", function (req, res) {
+  var newPrintJobs = printJobs.filter((job) => job.id > req.params.lastJobId);
+  res.send(newPrintJobs);
+});
 
-app.post('/api/receivePrintJob', function (req, res) {
-   let price = 0;
+app.post("/api/receivePrintJob", function (req, res) {
+  let price = 0;
 
-   const tableNumber = req.body.tableNumber;
-   const items = req.body.items;
-   const total = items.reduce((acc, item) => acc + item.price, 0);
+  const tableNumber = req.body.tableNumber;
+  const items = req.body.items;
+  const total = items.reduce((acc, item) => acc + item.price, 0);
 
-   const receiptText = `
+  const receiptText = `
       Table: ${tableNumber}\n\n
-      Items: ${items.map(item => item.name).join('\n')}
+      Items: ${items.map((item) => item.name).join("\n")}
       Total: ${total} EUR
    `;
 
-   var newJob = {
-      id: printJobs.length + 1,
-      name: req.query.name,
-      content: receiptText,
-   };
-   
-   printJobs.push(newJob);
+  var newJob = {
+    id: printJobs.length + 1,
+    name: req.query.name,
+    content: receiptText,
+  };
 
-   res.send('DK Print job received');
-})
+  printJobs.push(newJob);
 
-app.use(express.static('static'));
+  res.send("DK Print job received");
+});
 
+app.use(express.static("static"));
 
 var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("DK Print app listening at port", port)
-})
+  var host = server.address().address;
+  var port = server.address().port;
 
+  console.log("DK Print app listening at port", port);
+});
