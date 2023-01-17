3018fc1e143a2ebb7aa71ab7e3c77d603db8974e
John Dodds <john.dodds@populi.uk>
2023-01-17 16:33:26 +0100

    tweaks

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ app.use(express.json());
 var printJobs = [];
 
 app.get("/api/printJobsAfter/:lastJobId", function (req, res) {
+  console.log(printJobs);
   var newPrintJobs = printJobs.filter((job) => job.id > req.params.lastJobId);
+  console.log(newPrintJobs);
   res.send(newPrintJobs);
 });
 
 app.post("/api/receivePrintJob", function (req, res) {
   let price = 0;
+
+  console.log("/api/receivePrintJob", req.body);
+  console.log(req);
 
   const tableNumber = req.body.tableNumber;
   const items = req.body.items;
